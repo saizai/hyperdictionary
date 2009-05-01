cd2c8f78e48bbba99092d3ff025755efc12b0dae
Sai Emrys <github@saizai.com>
2009-05-01 05:31:34 -0700

    update ar-extensions initializer to new (poorly documented :() format

--- config/environment.rb
+++ config/environment.rb
@@ -64,6 +64,3 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
-
- require 'ar-extensions/adapters/mysql'
- require 'ar-extensions/import/mysql'
--- config/initializers/ar-extensions.rb
+++ config/initializers/ar-extensions.rb
@@ -1,3 +1,1 @@
-## Add these to AFTER the environment.rb initalize block. Unfortunately, they cause errors in an intializer.
-# require 'ar-extensions/adapters/mysql'
-# require 'ar-extensions/import/mysql'
+ENV['LOAD_ADAPTER_EXTENSIONS'] = 'mysql'

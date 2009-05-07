f109a8822f9d6b4b8c95a265fa9cc11930e9de71
Sai Emrys <github@saizai.com>
2009-05-07 02:18:54 -0700

    add icon thumbnails

--- db/migrate/20090507071529_add_icon_thumbnails.rb
+++ db/migrate/20090507071529_add_icon_thumbnails.rb
@@ -1,6 +1,6 @@
 class AddIconThumbnails < ActiveRecord::Migration
   def self.up
-    Asset.find_each(:batch_size => 5).map(&:save) # Trigger thumbnail creation
+    Asset.find_each(:batch_size => 5){|asset| asset.save} # Trigger thumbnail creation
   end
 
   def self.down

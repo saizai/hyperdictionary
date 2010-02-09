436b9d7ff02d49aeb402647146526f4c9b44fafb
Sai Emrys <github@saizai.com>
2010-02-09 00:48:41 -0800

    deprecations, default files

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '161037d25f46d5fd95beaf30623465b79ff9f10297a3c114fcb6414701f319bd4b874187f01e83cc223f99a10fa52a37a04d41c00b13dbbfa77312bc25037dfd'
